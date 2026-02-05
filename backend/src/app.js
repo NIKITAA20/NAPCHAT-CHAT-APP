7b939a7a804ef25b51e5b623e5dae482e4c363ae
Nikita Solanki <https://github.com/nikitajitendrasolanki>
2026-02-05 11:54:57 +0530

    Changes for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,13 @@ import chatRoutes from "./routes/chat.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
